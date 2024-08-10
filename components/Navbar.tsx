e8daa1617c6cc9717c2b915b47b8204747957fc5
Ahmad Syahbandi <saboonlbk@gmail.com>
2024-08-10 21:29:57 +0800

    reduce image and font

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,17 +4,17 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 const Navbar = async () => {
   const session = await auth();
   return (
-    <div className="flex items-center px-10 py-4 bg-white shadow shadow-neutral-200">
+    <div className="flex items-center px-10 py-4">
       <div className="ml-auto flex items-center gap-3">
-        <Avatar>
+        <Avatar className="h-9 w-9">
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>A</AvatarFallback>
         </Avatar>
         <div>
-          <div className="text-primary font-medium">
+          <div className="text-secondary-foreground font-semibold text-sm">
             {session?.user?.full_name}
           </div>
-          <div className="text-sm text-neutral-400">{session?.user?.role}</div>
+          <div className="text-xs text-neutral-400">{session?.user?.role}</div>
         </div>
       </div>
     </div>
