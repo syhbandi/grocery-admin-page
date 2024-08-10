f61f85f373d0890173441b9b9262c4f6df9cf4f1
Ahmad Syahbandi <saboonlbk@gmail.com>
2024-08-10 18:14:58 +0800

    add pagination

--- components/products/Table.tsx
+++ components/products/Table.tsx
@@ -11,6 +11,7 @@ import {
 import { Product } from "@/lib/types";
 import Link from "next/link";
 import { FiEdit2 } from "react-icons/fi";
+import CustomPagination from "../Pagination";
 
 type Props = {
   page?: string;
@@ -18,11 +19,19 @@ type Props = {
   search?: string;
 };
 
+type Response = {
+  data: Product[];
+  meta: {
+    per_page: number;
+    total: number;
+  };
+};
+
 const getProducts = async (
   page?: string,
   size?: string,
   search?: string
-): Promise<{ data: Product[] }> => {
+): Promise<Response> => {
   const url = `${process.env.API_URL}/products`;
   const params = new URLSearchParams();
 
@@ -43,42 +52,49 @@ const getProducts = async (
 const ProductsTable = async ({ page, size, search }: Props) => {
   const data = await getProducts(page, size, search);
   return (
-    <Table className="border border-neutral-200">
-      <TableHeader>
-        <TableRow>
-          <TableHead>ID</TableHead>
-          <TableHead>Name</TableHead>
-          <TableHead>Price</TableHead>
-          <TableHead>Stock</TableHead>
-          <TableHead>Actions</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {data.data.length ? (
-          data.data.map((product) => (
-            <TableRow key={product.id}>
-              <TableCell>{product.id}</TableCell>
-              <TableCell>{product.name}</TableCell>
-              <TableCell>{product.price}</TableCell>
-              <TableCell>{product.stock}</TableCell>
-              <TableCell>
-                <Link href={`/dashboard/products/${product.id}/update`}>
-                  <Button variant={"ghost"}>
-                    <FiEdit2 />
-                  </Button>
-                </Link>
+    <>
+      <Table className="border border-neutral-200">
+        <TableHeader>
+          <TableRow>
+            <TableHead>ID</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Stock</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.data.length ? (
+            data.data.map((product) => (
+              <TableRow key={product.id}>
+                <TableCell>{product.id}</TableCell>
+                <TableCell>{product.name}</TableCell>
+                <TableCell>{product.price}</TableCell>
+                <TableCell>{product.stock}</TableCell>
+                <TableCell>
+                  <Link href={`/dashboard/products/${product.id}/update`}>
+                    <Button variant={"ghost"}>
+                      <FiEdit2 />
+                    </Button>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={5} className="text-center">
+                No data Found
               </TableCell>
             </TableRow>
-          ))
-        ) : (
-          <TableRow>
-            <TableCell colSpan={5} className="text-center">
-              No data Found
-            </TableCell>
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
+          )}
+        </TableBody>
+      </Table>
+      <div className="mt-5">
+        <CustomPagination
+          totalPages={Math.ceil(data.meta.total / data.meta.per_page)}
+        />
+      </div>
+    </>
   );
 };
 
