ce6adfafca540c12fe58f941e0b66da0ff991f32
Ahmad Syahbandi <saboonlbk@gmail.com>
2024-08-11 17:11:27 +0800

    delete submit button

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import SignInButton from "@/components/SignInButton";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-secondary space-y-2 min-h-screen">
       <h1 className="text-4xl font-semibold">Welcome!</h1>
-      <p>Please Sign In to use the app</p>
-      <SignInButton />
+      <p>Use the app to manage the store</p>
+      <Link href={"/dashboard"}>
+        <Button>Go to App</Button>
+      </Link>
     </div>
   );
 };
