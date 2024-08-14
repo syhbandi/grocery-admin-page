326e2ccd654a672d879b3a70aeb99ed4fed05ca5
Ahmad Syahbandi <saboonlbk@gmail.com>
2024-08-14 10:10:58 +0800

    set error page

--- app/dashboard/error.tsx
+++ app/dashboard/error.tsx
@@ -1,6 +1,18 @@
 "use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
 const DashboardError = () => {
-  return <div>DashboardError</div>;
+  const router = useRouter();
+
+  return (
+    <div className="h-full flex flex-col bg-secondary items-center justify-center">
+      <h1 className="text-2xl font-semibold">Oops!</h1>
+      <p>Something went wrong, please try again in a second</p>
+      <Button onClick={() => router.refresh()}>Try Again</Button>
+    </div>
+  );
 };
 
 export default DashboardError;
