4057761719f59733499da68536b42185f55ac0d9
syhbandi <saboonlbk@gmail.com>
2024-10-19 13:23:36 +0800

    remove welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 const HomePage = () => {
-  return (
-    <div className="flex flex-col items-center justify-center bg-secondary space-y-2 min-h-screen">
-      <h1 className="text-4xl font-semibold">Welcome!</h1>
-      <p>Use the app to manage the store</p>
-      <Link href={"/dashboard"}>
-        <Button>Go to App</Button>
-      </Link>
-    </div>
-  );
+  redirect("/dashboard");
 };
 
 export default HomePage;
