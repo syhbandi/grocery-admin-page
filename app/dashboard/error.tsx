54c258caf6bb4767382c5457040ad8ab7af0c7be
syhbandi <saboonlbk@gmail.com>
2024-10-19 13:23:24 +0800

    set error page

--- app/dashboard/error.tsx
+++ app/dashboard/error.tsx
@@ -3,13 +3,14 @@
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 
-const DashboardError = () => {
+const DashboardError = ({ error }: { error: Error }) => {
   const router = useRouter();
 
   return (
     <div className="h-full flex flex-col bg-secondary items-center justify-center space-y-3">
       <h1 className="text-2xl font-semibold">Oops!</h1>
-      <p>Something went wrong, please try again in a second</p>
+      <p>Something went wrong, please try again</p>
+      <p>{error?.message}</p>
       <Button onClick={() => router.refresh()}>Try Again</Button>
     </div>
   );
